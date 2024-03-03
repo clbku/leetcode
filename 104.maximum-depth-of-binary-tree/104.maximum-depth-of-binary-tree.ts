3c8786f1ae32d5276f3e5404c1658a40d7066b67
lyhc <1611967@hcmut.edu.vn>
2024-03-03 23:58:36 +0700

    Add solution for Maximum Depth of Binary Tree problem

---
+++ 104.maximum-depth-of-binary-tree/104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=104 lang=typescript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if (!root) return 0;
+    if (root.left === null && root.right === null) return 1;
+    
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+// @lc code=end
+
