901aa27e1025b35e9a2ac017cc6f7e5bb5ac57d6
lyhc <1611967@hcmut.edu.vn>
2024-03-06 09:08:07 +0700

    Add solution for Binary Tree Preorder Traversal

---
+++ 144.binary-tree-preorder-traversal/144.binary-tree-preorder-traversal.ts
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=144 lang=typescript
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return [];
+
+    return [root.val, ...preorderTraversal(root.left), ...preorderTraversal(root.right)]
+};
+// @lc code=end
+
+const tree = new TreeNode(1)
+
+console.log(preorderTraversal(tree))
